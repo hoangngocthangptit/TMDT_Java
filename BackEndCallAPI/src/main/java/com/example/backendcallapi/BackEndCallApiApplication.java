36936cb880d6c5e9c49c7e7729c1a13370744d97
Hoàng Ngọc Thắng <thanghn@neo.vn>
2023-02-15 15:17:57 +0700

    building

---
+++ BackEndCallAPI/src/main/java/com/example/backendcallapi/BackEndCallApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendcallapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCallApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndCallApiApplication.class, args);
+    }
+
+}
